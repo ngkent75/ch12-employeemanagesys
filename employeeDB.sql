c248b46f232a880f7fc4b64b5e3df6e2609c0925
Nate Kent <ngkent75@gmail.com>
2021-03-17 01:17:21 -0400

    added necessary starting files, dowloaded packages, created tables

---
+++ employeeDB.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS employeeDB;
+CREATE DATABASE employeeDB;
+USE employeeDB;
+
+/* department */
+
+CREATE TABLE department(
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+/* role */
+
+CREATE TABLE role(
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary NUMERIC(12,2) NOT NULL,
+  department_id INT NOT NULL,
+  PRIMARY KEY (id)
+);
+
+/* employee */
+
+CREATE TABLE employee(
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT,
+  PRIMARY KEY (id)
+);
